50fd81dfe7fc8b32834edaeb092e8fdf15677132
Kyle Holzinger <kylelholzinger@gmail.com>
2015-12-13 15:54:26 -0500

    [KH] test removing and cloning the repo

--- hi.rb
+++ hi.rb
@@ -7,6 +7,8 @@ post '/' do
   @payload = JSON.parse(params[:payload])
 
   puts "The url: #{@payload["repository"]["git_url"]}"
-  puts JSON.pretty_generate(@payload)
+  # puts JSON.pretty_generate(@payload)
+  puts `rm -rf #{@payload["repository"]["name"]}`
+  puts `git clone #{@payload["repository"]["git_url"]}`
 end
 
