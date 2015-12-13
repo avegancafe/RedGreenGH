f422270795934e2e9e1c2d0c96a0ecf41386c983
Kyle Holzinger <kylelholzinger@gmail.com>
2015-12-13 15:49:44 -0500

    [KH]

--- hi.rb
+++ hi.rb
@@ -1,11 +1,6 @@
 require 'sinatra'
 require 'json'
 
-
-def process_pull_request(pull_request)
-  puts JSON.pretty_generate(pull_request)
-end
-
 post '/' do
   @payload = JSON.parse(params[:payload])
 
