a6e3f5f3c253ab81b20287d4c0721369805a77ef
Kyle Holzinger <kylelholzinger@gmail.com>
2015-12-13 15:49:10 -0500

    [KH]

--- hi.rb
+++ hi.rb
@@ -3,13 +3,12 @@ require 'json'
 
 
 def process_pull_request(pull_request)
-  # puts JSON.pretty_generate(pull_request)
+  puts JSON.pretty_generate(pull_request)
 end
 
 post '/' do
   @payload = JSON.parse(params[:payload])
 
-  process_pull_request(@payload)
   puts @payload["repository"]["git_url"]
 end
 
