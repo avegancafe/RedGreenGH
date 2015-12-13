7796d45b65f400cf04598e5ef27ba8e98099642d
Kyle Holzinger <kylelholzinger@gmail.com>
2015-12-13 15:51:29 -0500

    [KH]

--- hi.rb
+++ hi.rb
@@ -1,9 +1,12 @@
 require 'sinatra'
 require 'json'
 
+
+
 post '/' do
   @payload = JSON.parse(params[:payload])
 
   puts "The url: #{@payload["repository"]["git_url"]}"
+  puts JSON.pretty_generate(@payload)
 end
 
